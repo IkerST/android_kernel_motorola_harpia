3b58723ecb50a7aff9baaecfce0b4003350c5a02
Iker Sandí Trejo <35041658+IkerST@users.noreply.github.com>
2018-08-23 03:06:06 -0500

    Save make logs

--- harpia.sh
+++ harpia.sh
@@ -1,5 +1,5 @@
 KERNEL_DIR=$PWD
-Anykernel_DIR=$KERNEL_DIR/Anykernel2/harpia
+ANYKERNEL_DIR=$KERNEL_DIR/Anykernel2/harpia
 TOOLCHAINDIR=$(pwd)/toolchain/linaro-7.2
 DATE=$(date +"%d%m%Y")
 KERNEL_NAME="GKernel"
@@ -7,6 +7,7 @@ DEVICE="-harpia-"
 VER=$(cat version)
 TYPE="-OREO-"
 FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".zip
+LOG_FILE=FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".log
 
 export ARCH=arm
 export KBUILD_BUILD_USER="ist"
@@ -20,13 +21,14 @@ rm arch/arm/boot/zImage #Just to make sure it doesn't make flashable zip with pr
 fi;
 
 make harpia_defconfig
-make -j$( nproc --all )
+echo "Building with " $( nproc -all) " CPU(s)"
+make -j$( nproc --all ) 2>&1 | tee $LOG_FILE
 
-if [ -e  arch/arm/boot/zImage ];
+if [ -e  arch/arm/boot/zImage ]; 
 then
 echo "Kernel compilation completed"
-cp $KERNEL_DIR/arch/arm/boot/zImage $Anykernel_DIR/
-cd $Anykernel_DIR
+cp $KERNEL_DIR/arch/arm/boot/zImage $ANYKERNEL_DIR/
+cd $ANYKERNEL_DIR
 echo "Making Flashable zip"
 echo "Generating changelog"
 git log --graph --pretty=format:'%s' --abbrev-commit -n 200  > changelog.txt
